1a02227bfc839bd66300041f717b75c32e89bfaa
Allen Francis <lallenfrancisl@gmail.com>
2025-01-04 13:43:52 +0530

    feat: initial route setup

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {	
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Show a specific snippet by id
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet"))
+}
+
+// Create a new snippet
+func snippetCreate(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Create a snippet"))	
+}
 
 func main() {
-    fmt.Println("Hello World")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippets/view", snippetView)
+	mux.HandleFunc("/snippets/create", snippetCreate)
+
+	log.Println("starting server on localhost:4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
