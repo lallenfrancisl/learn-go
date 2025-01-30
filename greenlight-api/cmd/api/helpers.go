9547ce76fc481ab8bc7b18e365d5401a5370a962
Allen Francis <lallenfrancisl@gmail.com>
2025-01-30 21:59:15 +0530

    feat: add helpers for decoding errors and reading json

--- greenlight-api/cmd/api/helpers.go
+++ greenlight-api/cmd/api/helpers.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -48,6 +50,37 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	_, err = w.Write(js)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func (app *application) readJSON(
+	w http.ResponseWriter, r *http.Request, dst interface{},
+) error {
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		if errors.As(err, &syntaxError) {
+			return fmt.Errorf("body contains badly formed JSON (at character %d)", syntaxError.Offset)
+		} else if errors.Is(err, io.ErrUnexpectedEOF) {
+			return errors.New("body contains badly formed JSON")
+		} else if errors.As(err, &unmarshalTypeError) {
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+		} else if errors.Is(err, io.EOF) {
+			return errors.New("body must not be empty")
+		} else if errors.As(err, &invalidUnmarshalError) {
+			panic(err)
+		} else {
+			return err
+		}
 	}
 
 	return nil
